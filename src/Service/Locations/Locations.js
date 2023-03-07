1609b88c1d7a5fb8a6ae103c21888d2f8968c5f3
sango <Matyila2sango@gmail.com>
2023-03-07 11:48:25 +0200

    testing1....

--- src/Service/Locations/Locations.js
+++ src/Service/Locations/Locations.js
@@ -8,12 +8,9 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Locations = void 0;
-const axios_1 = __importDefault(require("axios"));
+const axios = require("axios");
 const cheerio = require("cheerio");
 class Locations {
     constructor() {
@@ -24,7 +21,7 @@ class Locations {
         };
         this.scrapLocations = (search) => __awaiter(this, void 0, void 0, function* () {
             this._locations.search_parameter = search;
-            let response = yield axios_1.default
+            let response = yield axios
                 .get(`https://www.accuweather.com/en/search-locations?query=${search}`)
                 .then((prom) => prom.data)
                 .then((results) => results);
